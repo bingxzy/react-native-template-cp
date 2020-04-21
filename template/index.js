929a67555db5207cbbc719075d961698b749f7f5
Stuart <stuart_cn@outlook.com>
2020-04-21 10:10:15 +0800

    update index.js (support redux)

--- template/index.js
+++ template/index.js
@@ -1,9 +1,17 @@
-/**
- * @format
- */
-
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
+import React from 'react';
 import App from './src/App';
-import {name as appName} from './app.json';
+import { name as appName } from './app.json';
+import store, { persistor } from './src/stores';
 
-AppRegistry.registerComponent(appName, () => App);
+const reduxApp = () => (
+  <Provider store={store}>
+    <PersistGate persistor={persistor}>
+      <App />
+    </PersistGate>
+  </Provider>
+);
+
+AppRegistry.registerComponent(appName, () => reduxApp);
