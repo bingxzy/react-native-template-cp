dddd90727dd63aef3ea337a597918ab6e08b2976
stuart <stuartsitu@ideaculture.com>
2021-04-02 11:26:50 +0800

    add Archive file

---
+++ template/.eslintrc.js
@@ -0,0 +1,65 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: [
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'react/jsx-filename-extension': [1, { extensions: ['.js', 'jsx'] }],
+    'react/prefer-stateless-function': [0],
+    'no-useless-concat': [0],
+    'no-use-before-define': [0],
+    'no-console': [0],
+    'react/prop-types': [0],
+    'no-plusplus': [0, { allowForLoopAfterthoughts: true }],
+    'react/jsx-boolean-value': [0],
+    'consistent-return': [0],
+    'global-require': [0],
+    'no-restricted-syntax': [0],
+    'no-bitwise': [0],
+    'prefer-destructuring': [0],
+    'no-unused-expressions': ['error', {
+      allowShortCircuit: true,
+      allowTernary: true,
+      allowTaggedTemplates: true,
+    }],
+    'no-useless-escape': [0],
+    'no-await-in-loop': [0],
+    'no-nested-ternary': [0],
+    'no-negated-condition': [0],
+    camelcase: [0],
+    'max-len': ['error', { code: 150 }],
+    'import/extensions': [0],
+    'no-empty': ['error', {
+      allowEmptyCatch: true,
+    }],
+    'no-underscore-dangle': [0],
+    'react/jsx-props-no-spreading': [0],
+    'no-shadow': [0],
+    'react/no-array-index-key': [0],
+    'no-param-reassign': [0],
+    'import/no-extraneous-dependencies': ['error',
+      {
+        devDependencies: true,
+        optionalDependencies: false,
+        peerDependencies: false,
+      }],
+  },
+  parser: 'babel-eslint',
+};
