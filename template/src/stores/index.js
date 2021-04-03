e0eb77e1d3bf9a51ec175bc3579b7dadfedec314
stuart <stuartsitu@ideaculture.com>
2021-04-03 12:40:22 +0800

    update version 1.0.1

--- template/src/stores/index.js
+++ template/src/stores/index.js
@@ -4,7 +4,7 @@ import {
 } from 'redux';
 import createSagaMiddleware from 'redux-saga';
 import { persistStore, persistReducer } from 'redux-persist';
-import AsyncStorage from '@react-native-community/async-storage';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import reducers from './reducer';
 import rootSagas from './sagas';
 
